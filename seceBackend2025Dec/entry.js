4ce6c778c35d0caf9cd7e601352aa8b81509a7b0
shivatharun <tharunshiva229@gmail.com>
2025-12-18 15:35:53 +0530

    8 commit

--- seceBackend2025Dec/entry.js
+++ seceBackend2025Dec/entry.js
@@ -2,21 +2,41 @@ const express = require("express");
 const mdb = require("mongoose");
 const Signup = require("./models/SignupSchema");
 const bcrypt = require("bcrypt");
-const cors = require("cors")
+const cors = require("cors");
+const dotenv = require("dotenv");
+const jwt = require('jsonwebtoken')
 const app = express();
 const PORT = 8001;
 
 app.use(express.json());
 app.use(cors())
+dotenv.config()
 
 mdb
-  .connect("mongodb+srv://cys:cys@cypersec.h6wsgqg.mongodb.net/seceDec2025")
+  .connect(process.env.MONGODB_URI)
   .then(() => console.log("MongoDB Connection Successful"))
   .catch((err) => console.log("MongoDB Connection Unsuccessful", err));
 
 app.get("/", (req, res) => {
   res.send("Server started successfully");
 });
+
+const verifyToken = (req,res,next) =>{
+  const token = req.headers.authorization
+  if(!token){
+    res.send("Request Denied")
+  }
+  try{
+    const payload = jwt.verify(token, process.env.SECRET_KEY)
+    console.log(payload);
+    console.log("Middleware check successful");
+    req.username = payload.username
+    next();
+  }
+  catch(err){
+    console.log(err);
+  }
+}
 
 app.post("/signup", async (req, res) => {
   const { email, username, password } = req.body;
@@ -37,6 +57,10 @@ app.post("/login", async (req, res) => {
     console.log(existingUser);
 
     if (existingUser) {
+      const payload = {
+        email:existingUser.email,
+        username:existingUser.username
+      }
       const isValidPassword = await bcrypt.compare(
         password,
         existingUser.password
@@ -44,9 +68,12 @@ app.post("/login", async (req, res) => {
       console.log(isValidPassword);
 
       if (isValidPassword) {
+        const token = jwt.sign(payload,process.env.SECRET_KEY)
+        console.log(token);
         res.status(200).json({
           message: "Login Successful",
           isLoggedIn: true,
+          token:token
         });
       } else {
         res.status(401).json({
@@ -76,19 +103,24 @@ app.get('/getallsignup',async(req,res)=>{
 
 })
 
-app.get("/json", (req, res) => {
+app.get("/json",verifyToken, (req, res) => {
   res.json({
     College: "Sece",
     Dept: "CYS",
     StuCount: "64",
+    fac:req.username
   });
 });
 
 app.get("/static", (req, res) => {
   res.sendFile(
-    "/Users/prasanthksp/Documents/RAMPeX-Parent-Folder/Trainings/SECE/SECE_MERN_DEC_2025/seceBackend2025Dec/index.html"
+    "C:\Users\tharu\MernIntern2025\seceBackend2025Dec\index.html"
   );
 });
+
+app.get('/personal-profile',verifyToken, (req,res)=>{
+  res.send(`Welcome, ${req.username}`)
+})
 
 app.listen(PORT, () => {
   console.log(`Server Started Successfully in the port ${PORT}`);
